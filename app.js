f99e95444c4a09b32be3a7d34043c84e4340b6d6
‘Michael <pickup.patterns@gmail.com>
2018-10-26 14:37:25 -0700

    problem with functionality of capturing a button press

--- app.js
+++ app.js
@@ -1,34 +1,53 @@
-/* global words onload */
+/* global words */
 /* exported onSubmit */
 /* exported loadGame */
 //initial empty array for individual letters
+var gameinit = words.slice();
+var index = getRandomIndex(words.length);
+var phrase = gameinit[index].split('');
 
 function loadGame() {
-    var onload = words.slice();
-    var index = getRandomIndex(words.length);
-    var phrase = onload[index].split('');
-    
+
     console.log(phrase);
     var output = document.getElementById('output');
     output.textContent = phrase.join(' ');
 }
 
+
 function onSubmit() {
+    var userGuess = document.getElementById('guess');
+    var elements = userGuess.elements;
 
-    // log submission into array
+    var guessInput = elements.guess;
+    var theirGuess = guessInput.value;
 
-    //if submit button contains letter that is in the word(array), show letter
-
-    //pop off letter from array .pop() - game is done when no more letters in array!
-
-    //if letter is not, create alert
-
-
+    
+    console.log(theirGuess);
+    
+    // var count = phrase.length;
+    // for (var i = 0; i < count; i++) {
+    //     if (phrase[i] === userGuess) {
+    //         console.log('good choice');
+    //     }
+    // }
+    // return false;
 }
+
+// log submission into array
+
+//if submit button contains letter that is in the word(array), show letter
+
+//pop off letter from array .pop() - game is done when no more letters in array!
+
+//if letter is not, create alert
+
+
+
 
 
 // based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIndex(length) {
     //The maximum is exclusive and the minimum (0) is inclusive
-    return Math.floor(Math.random() * length); 
+    return Math.floor(Math.random() * length);
 }
+    
