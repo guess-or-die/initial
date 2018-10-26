e79a6d9a4a977855d3516cc8b000000cdc6ff407
Paige Gorry <paigeegorry@gmail.com>
2018-10-26 13:39:15 -0700

    added onload function, creating a new function for the submit button

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 /* global words onload */
-/* exported getRandomWord */
+/* exported onSubmit */
+/* exported loadGame */
 //initial empty array for individual letters
-var cheesecake = [];
 
-
-function getRandomWord() {
+function loadGame() {
     var onload = words.slice();
     var index = getRandomIndex(words.length);
     var phrase = onload[index].split('');
-
-
+    
     console.log(phrase);
     var output = document.getElementById('output');
     output.textContent = phrase.join(' ');
-    
-    //in the output, phrase[0] returns first letter, 
-    //console.log(output); 
 }
-//need an empty array outside the for loop send to the  section id=output
+
+function onSubmit() {
+
+    // log submission into array
+
+    //if submit button contains letter that is in the word(array), show letter
+
+    //pop off letter from array .pop() - game is done when no more letters in array!
+
+    //if letter is not, create alert
 
 
-
-//similar working ref: https://stackoverflow.com/questions/8131838/returning-values-out-of-for-loop-in-javascript
-// function getId(a){
-    //     var aL = a.length;
-//     var values = [];
-//     for(i = 0; i < aL; i++ ){
-//       values.push(a[i][2].split(":", 1)[0]);
-//     }    
-//     return values.join('');
-//   } 
-//print letters container  to section id=output
-//add a function from user input to compare with word in the queue
-//get total number of turns
-//
+}
 
 
 // based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
