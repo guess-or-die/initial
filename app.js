af850b52726e43cbf78274a0b20e54f642adb889
‘Michael <pickup.patterns@gmail.com>
2018-10-26 16:06:39 -0700

    made some wrong turns but getting there.

--- app.js
+++ app.js
@@ -5,25 +5,33 @@
 var gameInit = words.slice();
 var index = getRandomIndex(words.length);
 var phrase = gameInit[index].split('');
+var linesplatz = phrase.length;
 
 function loadGame() {
-
     var output = document.getElementById('output');
-    output.textContent = phrase.join(' ');
+    //console.log(phrase);
+    //output.textContent = linesplatz;
+    
+    for(var i = 0; i < phrase.length; i++) {
+    //get the length of the word before gameplay and issue total number of tiles
+    
+
+    }
 }
 var guessTotals = [''];
 
 function onSubmit() {
     var userGuess = document.getElementById('answer-field');
     var elements = userGuess.elements;
-
+    
     var guessInput = elements.guess;
     var theirGuess = guessInput.value;
-
-    for(var i = 0; i < phrase.length; i++) {
+    
+    for(var i = 0; i < linesplatz; i++) {
         if(phrase[i] === theirGuess) {
             //console.log('good choice');
             guessTotals.push(phrase[i]);
+            output.textContent = phrase[i];
             console.log('this is your initial array ' + phrase);
             console.log('this is your return array ' + guessTotals);
         }
