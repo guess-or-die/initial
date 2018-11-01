f9d70817b83761f2e4d7ddbebc6f9c83412c0ea9
Paige Gorry <paigeegorry@gmail.com>
2018-11-01 08:22:34 -0700

    added some html and css styles; added reset to form so user can continuously guess new letters without the backspace.

--- app.js
+++ app.js
@@ -3,10 +3,10 @@
 /* exported loadGame */
 /* exported resetButton */
 
-//initial empty array for individual letters
 var gameInit = words.slice();
 var index = getRandomIndex(words.length);
 var phrase = gameInit[index].split('');
+var form = document.getElementById('answer-field');
 
 function loadGame() {
     console.log(phrase);
@@ -42,36 +42,22 @@ function onSubmit() {
     
     console.log(guessCount);
     
+    form.reset();
     
     var turnsTotal = document.getElementById('turns-total');
     var triesLeft = 6 - guessCount;
     turnsTotal.textContent = 'You have ' + triesLeft + ' tries left';
     if(guessCount === 6){
-        turnsTotal.textContent = 'TRY BETTER NEXT TIME LOSER';
-        
+        turnsTotal.textContent = 'TRY BETTER NEXT TIME LOSER'; 
     }   
-    console.log('this is your initial array ' + phrase);
     guessedLetters.push(theirGuess);
     var letterLog = document.getElementById('letters');
-    letterLog.textContent = guessedLetters.join(' ');  
-    
-
-    
+    letterLog.textContent = guessedLetters.join(' ');     
 }
- 
-console.log('this is your initial array ' + phrase);
-console.log('this is your return array ' + correctLetters);
-
 
 function resetButton() {
     userGuess.reset;
 }
-
-//if submit button contains letter that is in the word(array), show letter
-
-//splice off letter from array .pop() - game is done when no more letters in array!
-
-//if letter is not, create alert
 
 // based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIndex(length) {
